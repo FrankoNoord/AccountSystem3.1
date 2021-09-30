05b743116a0387868f257f7a6245430368d5cb38
Franko <frankovn@hotmail.co.uk>
2021-09-30 13:57:43 +0200

    SeES 4.3 19:31 of 50min video 10/11 Before putting in the List and Dto part in Account Type controller

---
+++ AccountSystemLogic/src/main/java/za/ac/nwu/logic/flow/FetchAccountTypeFlow.java
@@ -0,0 +1,7 @@
+package za.ac.nwu.logic.flow;
+
+import za.ac.nwu.ac.domain.persistence.AccountType;
+
+public interface FetchAccountTypeFlow {
+    //List<AccountTypeDto> getAllAccountTypes();
+}
--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -3,17 +3,26 @@ package za.ac.nwu.ac.web.sb.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import za.ac.nwu.ac.domain.service.GeneralResponse;
+//import za.ac.nwu.ac.logic.flow.FetchAccountTypeFlow;
+import za.ac.nwu.logic.flow.FetchAccountTypeFlow;
 
 @RestController
 @RequestMapping("account-type")
 public class AccountTypeController {
 
+    private final FetchAccountTypeFlow fetchAccountTypeFlow;
+
+    @Autowired
+    public AccountTypeController(FetchAccountTypeFlow fetchAccountTypeFlow){
+        this.fetchAccountTypeFlow=fetchAccountTypeFlow;
+    }
     @GetMapping("/all")
     @ApiOperation(value = "Get all the configured Account types.", notes = "Returns a list of account types")
     @ApiResponses(value = {
